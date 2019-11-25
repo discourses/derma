28c8d2280a8819b18a3e75d52ee1c888a2ff7c2d
ojoc <ojoc@users.noreply.github.com>
2019-11-25 15:53:22 +0000

    src/main.py: In progress

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import config
 
 import src.data.Sources as Sources
@@ -9,20 +8,18 @@ import sklearn.model_selection as model_selection
 
 def main():
 
-    print(os.getcwd())
-
     # Logging, logging.disable(logging.WARN)
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
     # Preliminaries
     random_state = config.variables['modelling']['parameters']['random_state']
-    listing, labels, features = Sources.Sources().summary()
+    listing, labels, fields = Sources.Sources().summary()
     xlearn, xtest, ylearn, ytest = model_selection\
         .train_test_split(listing.drop(columns=labels).values, listing[labels],
                           train_size=0.7, random_state=random_state, stratify=listing[labels])
 
-    logger.info(listing.head())
+    print(xlearn)
 
 
 if __name__ == '__main__':
