8c0c37d6908a90615b2e2f68329d1ffc3a183510
ojoc <ojoc@users.noreply.github.com>
2019-12-02 12:17:18 +0000

    tests/data/test_Sources.py: Corrected wrong assert expression

--- tests/data/test_Sources.py
+++ tests/data/test_Sources.py
@@ -20,5 +20,5 @@ class TestSources:
         assert n_metadata == listing.shape[0], "The number of medata & listing data points must be equal"
         assert len(labels) != 0, "At least one label column must exist"
         assert len(fields) != 0, "Missing field names"
-        assert listing.image.unique().shape[0] == listing.unique().shape[0], "Each image name can occur once only"
+        assert listing.image.unique().shape[0] == listing.shape[0], "Each image name can occur once only"
         assert listing[labels].sum(axis=1).all(), "Each image must be associated with a single class only"
