d599a3857fdb70feba71e47cfdfd27081b04ca59
ojoc <ojoc@users.noreply.github.com>
2019-12-05 00:51:12 +0000

    src/main.py: In progress

--- src/main.py
+++ src/main.py
@@ -1,29 +1,28 @@
 import logging
-import configurations.configurations as cfg
-
-import src.data.Usable as Usable
-import src.data.Images as Images
 
 import sklearn.model_selection as model_selection
 
+import configurations.configurations as cfg
+import src.data.images as images
+import src.data.usable as usable
+
 
 def main():
-
     # Logging
     cfg.logs()
     logger = logging.getLogger('root')
     logger.name = __name__
 
     # Preliminaries
-    listing, labels, fields = Usable.Usable().summary()
+    listing, labels, fields = usable.Usable().summary()
 
     # Split
     random_state = cfg.variables()['modelling']['parameters']['random_state']
-    xlearn, xtest, ylearn, ytest = model_selection\
+    xlearn, xtest, ylearn, ytest = model_selection \
         .train_test_split(listing.drop(columns=labels).values, listing[labels],
                           train_size=0.7, random_state=random_state, stratify=listing[labels])
 
-    x = Images.Images().states(listing['image'])
+    x = images.Images().states(listing['image'])
     logger.info(x)
 
 
