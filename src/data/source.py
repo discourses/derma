96c04d22610c5b657a2b915463c5117d5f373707
ojoc <ojoc@users.noreply.github.com>
2020-01-01 20:34:57 +0000

    src/data/source.py: Updating

--- src/data/source.py
+++ src/data/source.py
@@ -1,8 +1,6 @@
-import logging
 import os
 
 import pandas as pd
-import sklearn.model_selection as model_selection
 
 from src.cfg import cfg as cfg
 
@@ -14,22 +12,23 @@ class Source:
         # Root directory
         self.root = os.path.split(os.getcwd())[0]
 
-        # Logging
-        cfg.Cfg().logs()
-        logging.basicConfig(level=logging.DEBUG)
-        self.logger = logging.getLogger('root')
-        self.logger.name = __name__
-
         # Variables
         variables = cfg.Cfg().variables()
+
+        # Inventory
         self.inventory_filename = variables['inventory']['filename']
         self.inventory_url = variables['inventory']['url']
+        self.inventory_fields = variables['inventory']['fields']
+
+        # Images
         self.images_location = variables['images']['location']
         self.images_ext = variables['images']['ext']
-        self.inventory_fields = variables['inventory']['fields']
+
+        # Modelling
+        self.features = variables['modelling']['features']
         self.random_state = variables['modelling']['random_state']
-        self.train_size_inventory = variables['modelling']['train_size_inventory']
-        self.train_size_evaluation = variables['modelling']['train_size_evaluation']
+        self.sample = variables['modelling']['sample']
+        self.class_sample_size = variables['modelling']['class_sample_size']
 
 
     def inventory(self):
@@ -38,7 +37,12 @@ class Source:
         for i in self.inventory_filename:
             inventory_filename = os.path.join(inventory_filename, i)
 
-        return pd.read_csv(self.inventory_url)
+        inventory = pd.read_csv(inventory_filename)
+
+        inventory['name'] = inventory['image'] + inventory.angle.apply(
+            lambda x: '-' + str(x).zfill(3) + self.images_ext)
+
+        return inventory
 
 
     def url(self, inventory):
@@ -47,58 +51,34 @@ class Source:
         for i in self.images_location:
             images_location = os.path.join(images_location, i)
 
-        inventory['name'] = inventory['image'] + inventory.angle.apply(
-            lambda x: '-' + str(x).zfill(3) + self.images_ext)
         inventory['url'] = inventory.name.apply(lambda x: os.path.join(images_location, x))
 
         return inventory
 
 
-    def builder(self, x, y, labels, group):
-        """
-        :type x: numpy.ndarray
-        :type y: numpy.ndarray
-        :type labels: list
-        :type group: str
+    def sampling(self, data, fields, labels):
 
-        :param x: The features part of a data set
-        :param y: The corresponding classes/labels
-        :param labels: The list of the labels
-        :param group: The data group in focus, i.e., training, validating, or testing; for logging purposes.
-        :return:
-        """
+        excerpt = data.groupby(labels)[fields + labels] \
+            .apply(lambda x: x.sample(n=self.class_sample_size, replace=False, random_state=self.random_state))
+        excerpt.reset_index(drop=True, inplace=True)
 
-        data = pd.DataFrame(x, columns=['url']) \
-            .join(pd.DataFrame(y, columns=labels), how='inner')
-
-        self.logger.info("{}: {}, {}, {}".format(group, x.shape, y.shape, data.shape))
-
-        return data
+        return excerpt
 
 
     def summaries(self):
 
         # Read-in the inventory of images
         inventory = Source().inventory()
-        labels = inventory.columns.drop(self.inventory_fields)
+        fields = self.inventory_fields
+        labels = inventory.columns.drop(fields).tolist()
+
+        # Sample
+        if self.sample:
+            inventory = Source().sampling(data=inventory, fields=fields, labels=labels)
 
         # Add the images URL field
         inventory = Source().url(inventory)
 
-        # Stratified Splitting
-        x_learn, x_evaluation, y_learn, y_evaluation = model_selection. \
-            train_test_split(inventory[['url']], inventory[labels],
-                             train_size=self.train_size_inventory,
-                             random_state=self.random_state, stratify=inventory[labels])
-
-        x_validate, x_test, y_validate, y_test = model_selection. \
-            train_test_split(x_evaluation, y_evaluation,
-                             train_size=self.train_size_evaluation,
-                             random_state=self.random_state, stratify=y_evaluation)
-
-        # Setting-up for generator
-        training = Source().builder(x=x_learn, y=y_learn, labels=labels, group='Training')
-        validating = Source().builder(x=x_validate, y=y_validate, labels=labels, group='Validating')
-        testing = Source().builder(x=x_test, y=y_test, labels=labels, group='Testing')
-
-        return training, validating, testing, labels
+        # Return
+        features = self.features
+        return inventory, labels, features
