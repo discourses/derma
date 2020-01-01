a44098e5a39868c28ff7052a6be4a7cc6f96f00e
ojoc <ojoc@users.noreply.github.com>
2020-01-01 20:35:34 +0000

    src/main.py: in progress

--- src/main.py
+++ src/main.py
@@ -1,17 +1,40 @@
-import src.data.source as source
+import os
+import sys
 
-import src.modelling.VGG19E.simple as vgg19e
+if __name__ == '__main__':
+    sys.path.append(os.getcwd())
+    sys.path.append(os.path.join(os.getcwd(), 'src'))
+    import src.data.source as source
+    import src.data.transform as transform
+    import src.data.graphing as graphing
+
+    import src.modelling.VGG19E.simple as vgg19e_simple
+    import src.modelling.VGG19E.architecture as vgg19e_arc
 
 
 def main():
 
     # Data
-    training, validating, testing, labels = source.Source().summaries()
+    inventory, labels, features = source.Source().summaries()
+    training, validating, testing = transform.Transform().summaries(inventory, features, labels)
+
+    # Graphing
+    graphs = graphing.Graphing()
 
     # VGG19 Extract
-    vgg = vgg19e.Simple(training_=training, validating_=validating, testing_=testing,
-                        labels=labels, epochs=2)
-    vgg.run()
+    arc = vgg19e_arc.Architecture()
+    hyperparameters = arc.hyperparameters()
+
+    # Experiment
+    vgg = vgg19e_simple.Simple(training_=training, validating_=validating, testing_=testing,
+                               labels=labels, epochs=2)
+    print(hyperparameters[0])
+    history = vgg.run(hyperparameters=hyperparameters[0])
+
+    print(type(history))
+    print(history.history['val_auc'])
+
+    graphs.plot_metrics(history=history)
 
 
 if __name__ == '__main__':
