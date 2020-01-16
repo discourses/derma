eb709c0286c0fc76eadd984318ca09faeb0da94b
ojoc <ojoc@users.noreply.github.com>
2020-01-16 15:09:55 +0000

    Include methods for preparing Tensorflow Datasets
    According to Google the Tensorflow Datasets data input pipelines are more efficient than the keras options.
    
    * https://www.tensorflow.org/tutorials/load_data/images#load_using_tfdata
    
    Modified: [src/data/reader.py]

--- src/data/reader.py
+++ src/data/reader.py
@@ -5,7 +5,7 @@ import src.config as config
 
 class Reader:
 
-    def __init__(self, rescale=1./255):
+    def __init__(self, rescale=1. / 255):
         """
         :type rescale: float
 
@@ -18,7 +18,43 @@ class Reader:
         self.columns = variables['images']['columns']
         self.batch_size = variables['modelling']['batch_size']
 
-    def generator(self, data, labels):
+
+    @staticmethod
+    def image_decoder(img):
+
+        # Convert the compressed img to a 3D uint8 tensor
+        img = tf.image.decode_png(contents=img, channels=3)
+
+        # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+        img = tf.image.convert_image_dtype(img, tf.float32)
+
+        # Hence
+        return img
+
+
+    @staticmethod
+    def image_label_pairs(i, j):
+        img = tf.io.read_file(i)
+        img = Reader().image_decoder(img)
+        return img, j
+
+
+    def generator_tensorflow(self, data, labels):
+        filenames = data['url'].values
+        labelnames = data[labels].values
+
+        dataset = tf.data.Dataset.from_tensor_slices((filenames, labelnames))
+
+        # 'cache/training/log'
+        dataset = dataset.map(Reader().image_label_pairs, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.batch(batch_size=self.batch_size, drop_remainder=False)
+        dataset = dataset.cache()
+        dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+        return dataset
+
+
+    def generator_keras(self, data, labels):
         base = tf.keras.preprocessing.image.ImageDataGenerator(rescale=self.rescale)
         return base.flow_from_dataframe(dataframe=data,
                                         directory=None,
