6b60e36b185d5c51316e08d7d9e0c59c89464deb
ojoc <ojoc@users.noreply.github.com>
2020-01-17 21:29:35 +0000

    Remove the sampling method.  Import images path.
    The sampling method is now part of https://github.com/greyhypotheses/derma/blob/develop/src/data/sampling.py.  Required edits were made.
    
    Additionally, the path to the images is imported via the global variables dictionary.

--- src/data/source.py
+++ src/data/source.py
@@ -1,11 +1,11 @@
 """Module source"""
 import os
 import sys
-import typing
 
 import pandas as pd
 
 import src.config as config
+import src.data.sampling as sampling
 
 
 class Source:
@@ -23,9 +23,6 @@ class Source:
         Herein, the constructor initialises the global variables used by the methods of this class.
         """
 
-        # Root directory
-        self.root = os.path.split(os.getcwd())[0]
-
         # Variables
         variables = config.Config().variables()
 
@@ -34,15 +31,12 @@ class Source:
         self.inventory_fields = variables['inventory']['fields']
 
         # Images
-        self.images_location = variables['images']['location']
+        self.images_path = variables['images']['path']
         self.images_ext = variables['images']['ext']
 
         # Modelling
         self.features = variables['modelling']['features']
-        self.random_state = variables['modelling']['random_state']
         self.sample = variables['modelling']['sample']
-        self.replace = variables['modelling']['replace']
-        self.class_sample_size = variables['modelling']['class_sample_size']
 
 
     def inventory(self) -> pd.DataFrame:
@@ -59,9 +53,6 @@ class Source:
             print(error)
             sys.exit(1)
 
-        inventory['name'] = inventory['image'] + inventory.angle.apply(
-            lambda x: '-' + str(x).zfill(3) + self.images_ext)
-
         return inventory
 
 
@@ -76,48 +67,9 @@ class Source:
             pandas DataFrame inventory
         """
 
-        images_location = self.root
-        for i in self.images_location:
-            images_location = os.path.join(images_location, i)
-
-        inventory['url'] = inventory.name.apply(lambda x: os.path.join(images_location, x))
+        inventory['url'] = inventory.name.apply(lambda x: os.path.join(self.images_path, x))
 
         return inventory
-
-
-    def sampling(self, data: pd.DataFrame, fields: typing.List, labels: typing.List) -> pd.DataFrame:
-        """
-        Provides a sample of the data
-
-        :type data: pandas.DataFrame
-        :type fields: List
-        :type labels: List
-
-        :param data: A data frame of the data set wherefrom a sample is extract
-        :param fields: The fields of the data set, excluding the class/label columns
-        :param labels: The labels of the data set
-        :return:
-            A sample of data
-        """
-
-        # Counts the number of records per class/label
-        n_per_label = data[labels].sum(axis=0)
-
-        # If sampling without replacement is required, but one or more classes have fewer records than
-        # the requested sample size, the sample size is assigned as outlined below
-        if (not self.replace) & (n_per_label.min() < self.class_sample_size):
-            print("Because sampling without replacement has been requested, and to ensure a balanced "
-                  "data set, the sample size has been changed to the size of the smallest class")
-            class_sample_size = n_per_label.min()
-        else:
-            class_sample_size = self.class_sample_size
-
-        # Hence
-        excerpt = data.groupby(labels)[fields + labels] \
-            .apply(lambda x: x.sample(n=class_sample_size, replace=self.replace, random_state=self.random_state))
-        excerpt.reset_index(drop=True, inplace=True)
-
-        return excerpt
 
 
     def summaries(self):
@@ -129,7 +81,7 @@ class Source:
 
         # Sample
         if self.sample:
-            inventory = Source().sampling(data=inventory, fields=fields, labels=labels)
+            inventory = sampling.Sampling().sample(data=inventory, fields=fields, labels=labels)
 
         # Add the images URL field
         inventory = Source().url(inventory)
