eb3d0a649756cf6e2d5d1781e0ac0b0fb5f8b5aa
ojoc <ojoc@users.noreply.github.com>
2020-01-21 17:22:08 +0000

    Complete incomplete methods of src/evaluation/confusion.py
    Modified: [src/evaluation/confusion.py]

--- src/evaluation/confusion.py
+++ src/evaluation/confusion.py
@@ -1,11 +1,17 @@
 import numpy as np
 
+import src.config as config
+
 
 class Confusion:
 
     def __init__(self):
 
-        self.name = 'Confusion'
+        variables = config.Config().variables()
+
+        # Thresholds
+        thr = variables['evaluating']['thresholds']
+        self.thresholds = np.arange(start=thr['min'], stop=thr['max'], step=thr['step'])
 
 
     @staticmethod
@@ -23,7 +29,7 @@ class Confusion:
 
         n_per_class = instances.sum(axis=0, keepdims=True).squeeze(axis=0).tolist()
 
-        return n_per_class
+        return [threshold] + n_per_class
 
 
     def true_negative(self, threshold, plausibilities, truth):
@@ -32,7 +38,7 @@ class Confusion:
 
         n_per_class = instances.sum(axis=0, keepdims=True).squeeze(axis=0).tolist()
 
-        return n_per_class
+        return [threshold] + n_per_class
 
 
     def false_positive(self, threshold, plausibilities, truth):
@@ -41,7 +47,7 @@ class Confusion:
 
         n_per_class = instances.sum(axis=0, keepdims=True).squeeze(axis=0).tolist()
 
-        return n_per_class
+        return [threshold] + n_per_class
 
 
     def false_negative(self, threshold, plausibilities, truth):
@@ -50,17 +56,14 @@ class Confusion:
 
         n_per_class = instances.sum(axis=0, keepdims=True).squeeze(axis=0).tolist()
 
-        return n_per_class
+        return [threshold] + n_per_class
 
 
-    def calculate(self, plausibilities, truth, labels, path):
+    def calculate(self, plausibilities, truth, variate):
 
-        # Temporary.  Delete.
-        thresholds = np.arange(0, 0.9, 0.05)
-
-        # Example
-        [self.false_negative(i, plausibilities, truth) for i in thresholds]
-
-        # Write to CSV
-        print(labels)
-        print(path)
+        return {
+            'tn': [self.true_negative(i, plausibilities, truth) for i in self.thresholds],
+            'fn': [self.false_negative(i, plausibilities, truth) for i in self.thresholds],
+            'tp': [self.true_positive(i, plausibilities, truth) for i in self.thresholds],
+            'fp': [self.false_positive(i, plausibilities, truth) for i in self.thresholds]
+        }.get(variate, LookupError('{} could not be mapped to a function'.format(variate)))
