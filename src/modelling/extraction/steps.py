69edff254d58d9119f73faaa3704c7a651a6522f
ojoc <10231132+greyhypotheses@users.noreply.github.com>
2020-02-05 15:53:07 +0000

    Apply simple edits to src/modelling/extraction/steps.py
    Especially, the docstrings & variable hints.
    
    Modifies [src/modelling/extraction/steps.py]

--- src/modelling/extraction/steps.py
+++ src/modelling/extraction/steps.py
@@ -1,4 +1,7 @@
 import os
+import logging
+
+import pandas as pd
 
 import config
 import src.modelling.extraction.estimating as estimating
@@ -9,11 +12,21 @@ class Steps:
 
     def __init__(self):
 
+        # Logging
+        config.Config().logs()
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger('root')
+        self.logger.name = __name__
+
         # Variables
         variables = config.Config().variables()
         self.model_checkpoints_path = variables['modelling']['model_checkpoints_path']
 
     def cleanup(self):
+        """
+        Ensures that the hosting directories are empty
+        :return:
+        """
 
         files = [os.remove(os.path.join(base, file))
                  for base, directories, files in os.walk(self.model_checkpoints_path)
@@ -29,17 +42,27 @@ class Steps:
                 "Unable to delete all the items of {}".format(self.model_checkpoints_path))
 
     @staticmethod
-    def partitions(path):
+    def partitions(path: str):
         """
-
-        :param path:
+        Creates a directory partition
+        :param path: The directory that would be created if it doesn't exist
         :return:
         """
 
         if not os.path.exists(path):
             os.makedirs(path)
 
-    def proceed(self, labels, epochs, training_, validating_, testing_):
+    def evaluate(self, labels: list, epochs: int,
+                 training_: pd.DataFrame, validating_: pd.DataFrame, testing_: pd.DataFrame):
+        """
+        Runs one or more deep neural network models w.r.t. a defined architecture and sets of hyperparameters
+        :param labels:  The names of the label columns
+        :param epochs: The number of epochs
+        :param training_: The training data, which includes the 'url' column of local image file paths
+        :param validating_: The validation data, which includes the 'url' column of local image file paths
+        :param testing_: The testing data, which includes the 'url' column of local image file paths
+        :return:
+        """
 
         # Ensure that the checkpoints directory is empty.  And, just in case the
         # checkpoints directory is deleted, re-create it
@@ -54,14 +77,17 @@ class Steps:
 
         # Estimate a model per hyperparameter combination
         for i in range(len(hyp.values())):
+
             # Ensure that the model checkpoints for a combination of hyperparameters are saved
             # to a distinct directory that EXISTS
-            network_checkpoints_path = os.path.join(self.model_checkpoints_path, str(i).zfill(4))
+            hyperparameters_set: str = str(i).zfill(4)
+            network_checkpoints_path: str = os.path.join(self.model_checkpoints_path, hyperparameters_set)
             self.partitions(network_checkpoints_path)
 
-            print(hyp.values()[i])
+            hyperparameters_set_values: dict = hyp.values()[i]
+            self.logger.info(" Hyperparameters Set {}: {}".format(hyperparameters_set, hyperparameters_set_values))
 
             # History of losses
-            est.network(hyperparameters=hyp.values()[i], labels=labels, epochs=epochs,
+            est.network(hyperparameters=hyperparameters_set_values, labels=labels, epochs=epochs,
                         training_=training_, validating_=validating_, testing_=testing_,
                         network_checkpoints_path=network_checkpoints_path)
