e9a256cc72bfbda71070a22b38a95fefa727a996
ojoc <10231132+greyhypotheses@users.noreply.github.com>
2020-02-12 12:33:05 +0000

    Update the design of extraction/hyperparameters.py
    The hyperparameters values will now be served to a model via a package input argument.  The input argument is a URL string, the string is the path to a YAML file of hyperparameters of the form
    
    https://github.com/greyhypotheses/hub/blob/develop/data/hyper/pattern.yml
    
    Modifies: [src/modelling/extraction/hyperparameters.py]

--- src/modelling/extraction/hyperparameters.py
+++ src/modelling/extraction/hyperparameters.py
@@ -17,31 +17,37 @@ class Hyperparameters:
     whereby n is the number of values that should be sampled from the range.
     """
 
-    def __init__(self):
+    def __init__(self, hyperparameters_dict: dict):
         """
         Herein the constructor initialises global variables
         """
-        variables: dict = config.Config().variables()
+        # variables: dict = config.Config().variables()
 
-        model_extraction: dict = variables['model']['extraction']
-        self.num_units: list = model_extraction['num_units']
-        self.dropout: list = model_extraction['dropout']
-        self.opt: list = model_extraction['opt']
+        # model_extraction: dict = variables['model']['extraction']
+        # self.num_units: list = model_extraction['num_units']
+        # self.dropout: list = model_extraction['dropout']
+        # self.opt: list = model_extraction['opt']
+
+        self.alpha_units = hyperparameters_dict['alpha']['units']
+        self.alpha_dropout = hyperparameters_dict['alpha']['dropout']
+        self.beta_units = hyperparameters_dict['beta']['units']
+        self.beta_dropout = hyperparameters_dict['beta']['dropout']
+        self.opt: list = hyperparameters_dict['opt']
 
     def priors(self) -> (hp.HParam, hp.HParam, hp.HParam, hp.HParam, hp.HParam):
         """
         Initialises the set of values per hyperparameter type
         :return:
         """
-        alpha_units = hp.HParam('num_units', hp.Discrete(self.num_units))
-        alpha_drop_rate = hp.HParam('dropout', hp.Discrete(self.dropout))
+        alpha_units = hp.HParam('num_units', hp.Discrete(self.alpha_units))
+        alpha_dropout = hp.HParam('dropout', hp.Discrete(self.alpha_dropout))
 
-        beta_units = hp.HParam('num_units', hp.Discrete(self.num_units))
-        beta_drop_rate = hp.HParam('dropout', hp.Discrete(self.dropout))
+        beta_units = hp.HParam('num_units', hp.Discrete(self.beta_units))
+        beta_dropout = hp.HParam('dropout', hp.Discrete(self.beta_dropout))
 
         optimization = hp.HParam('optimizer', hp.Discrete(self.opt))
 
-        return alpha_units, alpha_drop_rate, beta_units, beta_drop_rate, optimization
+        return alpha_units, alpha_dropout, beta_units, beta_dropout, optimization
 
     def values(self) -> list:
         """
@@ -50,17 +56,17 @@ class Hyperparameters:
             combinations: A list of dictionaries, wherein each dictionary is a unique combination
             of hyperparameters.  Each combination estimates a distinct/single model.
         """
-        alpha_units, alpha_drop_rate, beta_units, beta_drop_rate, optimization = self.priors()
+        alpha_units, alpha_dropout, beta_units, beta_dropout, optimization = self.priors()
 
-        combinations = [{'alpha_drop_rate': i,
+        combinations = [{'alpha_dropout': i,
                          'alpha_units': j,
-                         'beta_drop_rate': x,
+                         'beta_dropout': x,
                          'beta_units': y,
                          'optimization': opt}
 
-                        for i in alpha_drop_rate.domain.values
+                        for i in alpha_dropout.domain.values
                         for j in alpha_units.domain.values
-                        for x in beta_drop_rate.domain.values
+                        for x in beta_dropout.domain.values
                         for y in beta_units.domain.values
                         for opt in optimization.domain.values]
 
