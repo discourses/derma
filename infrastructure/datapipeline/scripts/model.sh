518777c12b9e4085cde7acbc9d00a0537bdae81a
ojoc <10231132+greyhypotheses@users.noreply.github.com>
2020-02-13 18:37:27 +0000

    Update infrastructure/datapipeline/scripts/model.sh
    The docker command should be preceded by 'sudo'
    
    Modifies: [infrastructure/datapipeline/scripts/model.sh]

--- infrastructure/datapipeline/scripts/model.sh
+++ infrastructure/datapipeline/scripts/model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~
-docker run -v ~/images:/app/images -v ~/checkpoints:/app/checkpoints greyhypotheses/derma:FeatureExtractionDL src/main.py $1
+sudo docker run -v ~/images:/app/images -v ~/checkpoints:/app/checkpoints greyhypotheses/derma:FeatureExtractionDL src/main.py $1
