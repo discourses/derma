2c88bede01ec6bae9f5ac584edb175c31031ca2c
ojoc <10231132+greyhypotheses@users.noreply.github.com>
2020-03-02 22:38:23 +0000

    Update infrastructure/datapipeline/scripts/model.sh comments
    Modifies: [infrastructure/datapipeline/scripts/model.sh]

--- infrastructure/datapipeline/scripts/model.sh
+++ infrastructure/datapipeline/scripts/model.sh
@@ -3,5 +3,5 @@ cd ~
 
 # Runs the FeatureExtractionDL model.  It requires one string argument; the string
 # must be a URL oF A  YAML file of hyperparameters, e.g.,
-# https://raw.githubusercontent.com/greyhypotheses/hub/develop/data/hyper/pattern.yml
+# https://raw.githubusercontent.com/greyhypotheses/dictionaries/develop/derma/hyperparameters/pattern.yml
 sudo docker run -v ~/images:/app/images -v ~/checkpoints:/app/checkpoints greyhypotheses/derma:FeatureExtractionDL src/main.py $1
